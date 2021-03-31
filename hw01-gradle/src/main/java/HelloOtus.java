d5c853443bd0edb6bc7ed7304682111f565729c2
Amlet86 <wecow@yandex.ru>
2021-03-31 13:05:27 +0300

    hw01-gradle

---
+++ hw01-gradle/src/main/java/HelloOtus.java
@@ -0,0 +1,15 @@
+import static com.google.common.base.Preconditions.checkArgument;
+
+public class HelloOtus {
+
+    public static void main(String[] args) {
+        String someString = "qwerty";
+        guavaExample(someString);
+    }
+
+    private static void guavaExample(String someString) {
+        checkArgument(someString.length() > 0);
+        System.out.println("SUCCESS");
+    }
+
+}
