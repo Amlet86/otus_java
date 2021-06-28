53983a25cc361101171acdeb8ef5b2553bd123d3
Amlet86 <wecow@yandex.ru>
2021-06-28 10:06:38 +0300

    hw09-jdbc

---
+++ hw09-jdbc/homework/src/main/java/ru/amlet/jdbc/mapper/DataTemplateJdbc.java
@@ -0,0 +1,102 @@
+package ru.amlet.jdbc.mapper;
+
+import ru.amlet.core.repository.DataTemplate;
+import ru.amlet.core.repository.DataTemplateException;
+import ru.amlet.core.repository.executor.DbExecutor;
+
+import java.lang.reflect.Field;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Сохратяет объект в базу, читает объект из базы
+ */
+public class DataTemplateJdbc<T> implements DataTemplate<T> {
+
+    private final DbExecutor dbExecutor;
+    private final EntitySQLMetaData entitySQLMetaData;
+    private final EntityClassMetaData entityClassMetaData;
+
+    public DataTemplateJdbc(DbExecutor dbExecutor, EntitySQLMetaData entitySQLMetaData, EntityClassMetaData entityClassMetaData) {
+        this.dbExecutor = dbExecutor;
+        this.entitySQLMetaData = entitySQLMetaData;
+        this.entityClassMetaData = entityClassMetaData;
+    }
+
+    @Override
+    public Optional<T> findById(Connection connection, long id) {
+        return dbExecutor.executeSelect(connection, entitySQLMetaData.getSelectByIdSql(), List.of(id), rs -> {
+            try {
+                if (rs.next()) {
+                    return createInstance(rs);
+                }
+                return null;
+            } catch (Exception e) {
+                throw new DataTemplateException(e);
+            }
+        });
+    }
+
+    @Override
+    public List<T> findAll(Connection connection) {
+        return dbExecutor.executeSelect(connection, entitySQLMetaData.getSelectAllSql(), Collections.emptyList(), rs -> {
+            List<T> clientList = new ArrayList<T>();
+            try {
+                while (rs.next()) {
+                    clientList.add(createInstance(rs));
+                }
+                return clientList;
+            } catch (Exception e) {
+                throw new DataTemplateException(e);
+            }
+        }).orElseThrow(() -> new RuntimeException("Error"));
+    }
+
+    @Override
+    public long insert(Connection connection, T client) {
+        try {
+            List<Object> values = extractValues(client);
+            return dbExecutor.executeStatement(connection, entitySQLMetaData.getInsertSql(), values);
+        } catch (Exception e) {
+            throw new DataTemplateException(e);
+        }
+    }
+
+    @Override
+    public void update(Connection connection, T client) {
+        try {
+            List<Object> values = extractValues(client);
+            dbExecutor.executeStatement(connection, entitySQLMetaData.getUpdateSql(), values);
+        } catch (Exception e) {
+            throw new DataTemplateException(e);
+        }
+    }
+
+    private List<Object> extractValues(T object) throws IllegalAccessException {
+        List<Field> fields = entityClassMetaData.getFieldsWithoutId();
+        List<Object> values = new ArrayList<>(fields.size());
+
+        for (Field field : fields) {
+            field.setAccessible(true);
+            values.add(field.get(object));
+        }
+        return values;
+    }
+
+    private T createInstance(ResultSet rs) throws Exception {
+        Object instance = entityClassMetaData.getConstructor().newInstance();
+
+        for (Field field : entityClassMetaData.getAllFields()) {
+            Object value = rs.getObject(field.getName());
+            field.setAccessible(true);
+            field.set(instance, value);
+        }
+
+        return (T) instance;
+    }
+
+}
---
+++ hw09-jdbc/homework/src/main/java/ru/amlet/jdbc/mapper/EntitySQLMetaDataImpl.java
@@ -0,0 +1,66 @@
+package ru.amlet.jdbc.mapper;
+
+import java.lang.reflect.Field;
+import java.util.stream.Collectors;
+
+public class EntitySQLMetaDataImpl implements EntitySQLMetaData {
+
+    private final EntityClassMetaData entityClassMetaData;
+
+    public EntitySQLMetaDataImpl(EntityClassMetaData entityClassMetaData) {
+        this.entityClassMetaData = entityClassMetaData;
+    }
+
+    @Override
+    public String getSelectAllSql() {
+        return String.format("SELECT * FROM %s",
+            entityClassMetaData.getName().toLowerCase());
+    }
+
+    @Override
+    public String getSelectByIdSql() {
+        return String.format("SELECT %s FROM %s WHERE %s = ?",
+            getAllFields(),
+            entityClassMetaData.getName().toLowerCase(),
+            entityClassMetaData.getIdField().getName().toLowerCase());
+    }
+
+    private String getAllFields() {
+        return entityClassMetaData.getAllFields().stream()
+            .map(Field::getName)
+            .collect(Collectors.joining(","))
+            .toLowerCase();
+    }
+
+    @Override
+    public String getInsertSql() {
+        return String.format("INSERT INTO %s (%s) VALUES (%s)",
+            entityClassMetaData.getName().toLowerCase(),
+            getFieldsWithoutIdForInsert(),
+            entityClassMetaData.getFieldsWithoutId().stream()
+                .map(sql -> "?")
+                .collect(Collectors.joining(",")));
+    }
+
+    private String getFieldsWithoutIdForInsert() {
+        return entityClassMetaData.getFieldsWithoutId().stream()
+            .map(Field::getName)
+            .collect(Collectors.joining(","))
+            .toLowerCase();
+    }
+
+    @Override
+    public String getUpdateSql() {
+        return String.format("UPDATE %s SET %s WHERE %s = ?",
+            entityClassMetaData.getName().toLowerCase(),
+            getFieldsWithoutIdForUpdate(),
+            entityClassMetaData.getIdField().getName().toLowerCase());
+    }
+
+    private String getFieldsWithoutIdForUpdate() {
+        return entityClassMetaData.getFieldsWithoutId().stream()
+            .map(field -> field.getName() + " = ?")
+            .collect(Collectors.joining(","))
+            .toLowerCase();
+    }
+}
