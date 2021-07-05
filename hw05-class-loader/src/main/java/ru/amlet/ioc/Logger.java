2e58d53508e41032d3193c32ffe54c6f284132a2
Amlet86 <wecow@yandex.ru>
2021-07-05 15:46:04 +0300

    hw05-class-loader-aop

---
+++ hw05-class-loader/src/main/java/ru/amlet/ioc/Logger.java
@@ -0,0 +1,10 @@
+package ru.amlet.ioc;
+
+public interface Logger {
+
+    void calculation(int param1);
+
+    void calculation(int param1, int param2);
+
+    void calculation(int param1, int param2, String param3);
+}
